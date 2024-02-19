93bc61dc1159e1f04c5efab4548cac9a0f780c4f
Sheep1sik <bohg6799@naver.com>
2024-02-19 18:55:40 +0900

    Feat: JSON file Decode 및 Data 가져오는 기능 구현

---
+++ Africa/Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by 양원식 on 2024/02/19.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
